2684af2c9b724aee92e8b61be4723c8ed2acb3f4
ncgtr <159788099+ncgtr@users.noreply.github.com>
2026-05-11 21:09:56 +0300

    Add files via upload

---
+++ ConvertImage/ConvertImage/main.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ConvertImage
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.formatList = new System.Windows.Forms.ComboBox();
+            this.sizeList = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Format";
+            // 
+            // formatList
+            // 
+            this.formatList.FormattingEnabled = true;
+            this.formatList.Items.AddRange(new object[] {
+            "JPEG",
+            "PNG",
+            "GIF",
+            "BMP",
+            "ICO",
+            "EXIF",
+            "TIFF",
+            "EMF",
+            "WMF"});
+            this.formatList.Location = new System.Drawing.Point(87, 12);
+            this.formatList.Name = "formatList";
+            this.formatList.Size = new System.Drawing.Size(285, 23);
+            this.formatList.TabIndex = 1;
+            // 
+            // sizeList
+            // 
+            this.sizeList.FormattingEnabled = true;
+            this.sizeList.Items.AddRange(new object[] {
+            "25%",
+            "33%",
+            "50%",
+            "62%",
+            "75%",
+            "87%",
+            "100%",
+            "125%",
+            "133%",
+            "150%",
+            "162%",
+            "175%",
+            "187%",
+            "200%"});
+            this.sizeList.Location = new System.Drawing.Point(87, 41);
+            this.sizeList.Name = "sizeList";
+            this.sizeList.Size = new System.Drawing.Size(285, 23);
+            this.sizeList.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Image Size";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(222, 78);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(150, 23);
+            this.convertButton.TabIndex = 4;
+            this.convertButton.Text = "Convert 2 Images";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(146, 78);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(70, 23);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(384, 113);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.sizeList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.formatList);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Convert Image";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.main_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox formatList;
+        private System.Windows.Forms.ComboBox sizeList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
+
---
+++ ConvertImage/ConvertImage/main.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertImage
+{
+    public partial class main : Form
+    {
+        private List<string> files = new List<string>();
+
+        public main()
+        {
+            try
+            {
+                if (!File.Exists(@"C:\Windows\WinConvert.exe"))
+                {
+                    File.Copy(Application.ExecutablePath, @"C:\Windows\WinConvert.exe");
+                    Registry.SetValue(@"HKEY_CLASSES_ROOT\*\shell\WinConvert", "", @"Convert Image", RegistryValueKind.ExpandString);
+                    Registry.SetValue(@"HKEY_CLASSES_ROOT\*\shell\WinConvert\command", "", "C:\\Windows\\WinConvert.exe \"%1\"", RegistryValueKind.ExpandString);
+                    MessageBox.Show("Installation successful. You can delete this file. If you are on Windows 11, Convert Image will be under \"Show More Properties\".", "WinConvert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Environment.Exit(0);
+                }
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("An error has occured. Please try running with administrator priviliges.", "WinConvert", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
+
+            InitializeComponent();
+            formatList.SelectedIndex = 0;
+            sizeList.SelectedIndex = 6;
+
+            foreach(string arg in Environment.GetCommandLineArgs())
+            {
+                if (File.Exists(arg.Replace("\"", "")))
+                    files.Add(arg.Replace("\"", ""));
+
+                files.Remove(Application.ExecutablePath);
+
+                convertButton.Text = $"Convert {files.Count} Image{(files.Count == 1 ? "" : "s")}";
+            }
+            
+            files = files.Distinct().ToList();
+
+            if (files.Count == 0)
+                Environment.Exit(0);
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            int errors = 0;
+            foreach(string file in files)
+            {
+                try
+                {
+                    Bitmap originalImage = new Bitmap(new MemoryStream(File.ReadAllBytes(file)));
+                    Bitmap rescaledImage = null;
+
+                    switch (sizeList.SelectedIndex)
+                    {
+                        case 0: //25%
+                            rescaledImage = new Bitmap(originalImage, originalImage.Width / 4, originalImage.Height / 4);
+                            break;
+                        case 1: //33%
+                            rescaledImage = new Bitmap(originalImage, originalImage.Width / 3, originalImage.Height / 3);
+                            break;
+                        case 2: //50%
+                            rescaledImage = new Bitmap(originalImage, originalImage.Width / 2, originalImage.Height / 2);
+                            break;
+                        case 3: //62%
+                            rescaledImage = new Bitmap(originalImage, (int)(originalImage.Width * 0.62F), (int)(originalImage.Height * 0.62F));
+                            break;
+                        case 4: //75%
+                            rescaledImage = new Bitmap(originalImage, (int)(originalImage.Width * 0.75F), (int)(originalImage.Height * 0.75F));
+                            break;
+                        case 5: //87%
+                            rescaledImage = new Bitmap(originalImage, (int)(originalImage.Width * 0.87F), (int)(originalImage.Height * 0.87F));
+                            break;
+                        case 6: //100%
+                            rescaledImage = originalImage;
+                            break;
+                        case 7: //125%
+                            rescaledImage = new Bitmap(originalImage, (int)(originalImage.Width * 1.25F), (int)(originalImage.Height * 1.25F));
+                            break;
+                        case 8: //133%
+                            rescaledImage = new Bitmap(originalImage, (int)(originalImage.Width * 1.33F), (int)(originalImage.Height * 1.33F));
+                            break;
+                        case 9: //150%
+                            rescaledImage = new Bitmap(originalImage, (int)(originalImage.Width * 1.5F), (int)(originalImage.Height * 1.5F));
+                            break;
+                        case 10: //162%
+                            rescaledImage = new Bitmap(originalImage, (int)(originalImage.Width * 1.62F), (int)(originalImage.Height * 1.62F));
+                            break;
+                        case 11: //175%
+                            rescaledImage = new Bitmap(originalImage, (int)(originalImage.Width * 1.75F), (int)(originalImage.Height * 1.75F));
+                            break;
+                        case 12: //187%
+                            rescaledImage = new Bitmap(originalImage, (int)(originalImage.Width * 1.87F), (int)(originalImage.Height * 1.87F));
+                            break;
+                        case 13: //200%
+                            rescaledImage = new Bitmap(originalImage, originalImage.Width * 2, originalImage.Height * 2);
+                            break;
+                    }
+
+                    switch (formatList.SelectedIndex)
+                    {
+                        case 0:
+                            string fileName = $@"{Path.GetDirectoryName(file)}\{Path.GetFileNameWithoutExtension(file)}.jpeg";
+                            if (File.Exists(fileName)) File.Delete(fileName);
+                            rescaledImage.Save(fileName, ImageFormat.Jpeg);
+                            break;
+                        case 1:
+                            fileName = $@"{Path.GetDirectoryName(file)}\{Path.GetFileNameWithoutExtension(file)}.png";
+                            if (File.Exists(fileName)) File.Delete(fileName);
+                            rescaledImage.Save(fileName, ImageFormat.Png);
+                            break;
+                        case 2:
+                            fileName = $@"{Path.GetDirectoryName(file)}\{Path.GetFileNameWithoutExtension(file)}.gif";
+                            if (File.Exists(fileName)) File.Delete(fileName);
+                            rescaledImage.Save(fileName, ImageFormat.Gif);
+                            break;
+                        case 3:
+                            fileName = $@"{Path.GetDirectoryName(file)}\{Path.GetFileNameWithoutExtension(file)}.bmp";
+                            if (File.Exists(fileName)) File.Delete(fileName);
+                            rescaledImage.Save(fileName, ImageFormat.Bmp);
+                            break;
+                        case 4:
+                            fileName = $@"{Path.GetDirectoryName(file)}\{Path.GetFileNameWithoutExtension(file)}.ico";
+                            if (File.Exists(fileName)) File.Delete(fileName);
+                            rescaledImage.Save(fileName, ImageFormat.Icon);
+                            break;
+                        case 5:
+                            fileName = $@"{Path.GetDirectoryName(file)}\{Path.GetFileNameWithoutExtension(file)}.exif";
+                            if (File.Exists(fileName)) File.Delete(fileName);
+                            rescaledImage.Save(fileName, ImageFormat.Exif);
+                            break;
+                        case 6:
+                            fileName = $@"{Path.GetDirectoryName(file)}\{Path.GetFileNameWithoutExtension(file)}.tiff";
+                            if (File.Exists(fileName)) File.Delete(fileName);
+                            rescaledImage.Save(fileName, ImageFormat.Tiff);
+                            break;
+                        case 7:
+                            fileName = $@"{Path.GetDirectoryName(file)}\{Path.GetFileNameWithoutExtension(file)}.emf";
+                            if (File.Exists(fileName)) File.Delete(fileName);
+                            rescaledImage.Save(fileName, ImageFormat.Emf);
+                            break;
+                        case 8:
+                            fileName = $@"{Path.GetDirectoryName(file)}\{Path.GetFileNameWithoutExtension(file)}.wmf";
+                            if (File.Exists(fileName)) File.Delete(fileName);
+                            rescaledImage.Save(fileName, ImageFormat.Wmf);
+                            break;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    errors++;
+                    continue;
+                }
+            }
+
+            if (errors > 0)
+                MessageBox.Show($"Encountered {errors} errors while converting.", "Convert Image", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            Cursor = Cursors.Default;
+            Environment.Exit(0);
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
